68f5a8169643e76c8b2356c9cd73832ebe712f84
malygin04 <malygin04@yandex.ru>
2023-09-29 20:43:35 +0500

    lab02
    lab02

---
+++ Фт-220007-Малыгин.py
@@ -0,0 +1,8 @@
+# Задание 1
+a = int(input('Введите число a: '))
+b = int(input('Введите число b: '))
+print(a + b)
+print(a - b)
+print(a * b)
+print((a + b)/2)
+print(max(abs(a), abs(b)) - min(abs(a), abs(b)))
